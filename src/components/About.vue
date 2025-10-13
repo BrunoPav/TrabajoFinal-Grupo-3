903a96247d913b7d5025e47f4fbf72a324e46c09
Nacho Lamalfa <nacholamalfa@gmail.com>
2025-10-13 18:34:39 -0300

    Configure vue Router y agregue home.js y about.js

---
+++ src/components/About.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>About</h2>
+  <p>Esta es la página About.</p>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+  
+];
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use (router);
+app.mount('#app');
+
 
 
 const pruebaCommit = "Sebastian: probandoo"
