b0eeb1a22974eaf632ae5e558e376e695a64321a
Nacho Lamalfa <nacholamalfa@gmail.com>
2025-10-21 19:35:21 -0300

    Movi configuracionde main.js a router.js

--- src/main.js
+++ src/main.js
@@ -1,32 +1,6 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
-import Home from './components/Home.vue'
-import About from './components/About.vue'
+import router from './router'
 
-const routes = [
-  {
-    path: '/',
-    component: Home,
-  },
-  {
-    path: '/about',
-    component: About,
-  },
-  
-];
-
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
-
-const app = createApp(App);
-app.use (router);
-app.mount('#app');
-
-
-
-const pruebaCommit = "Sebastian: probandoo"
+createApp(App).use(router).mount('#app')
