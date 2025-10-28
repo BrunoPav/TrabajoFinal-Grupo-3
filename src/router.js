10c11177b6a288dc2c39637a1f6c861e5a8dfb6c
Nacho Lamalfa <nacholamalfa@gmail.com>
2025-10-28 15:51:50 -0300

    Actualizo router.js

--- src/router.js
+++ src/router.js
@@ -3,8 +3,12 @@ import Home from './components/Home.vue'
 import About from './components/About.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
+  { path: '/',
+     component: Home 
+    },
+  { path: '/about', 
+    component: About 
+},
 ]
 
 const router = createRouter({
